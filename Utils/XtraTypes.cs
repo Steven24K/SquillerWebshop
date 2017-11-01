e9a6dbd6af375403d7e30da1f38e03a8865f5b6f
Steven <s.koerts2@gmail.com>
2017-11-01 13:42:24 +0100

    Add orderstatus type

--- Utils/XtraTypes.cs
+++ Utils/XtraTypes.cs
@@ -2,5 +2,5 @@ namespace Webshop.Utils.Xtratypes{
     public enum Gender{MAN, WOMAN, UNSPECIFIED, UNISEX} //In the file Xtensions there is an explicit conversion from GenderToString, when added
     //a new gender to the enum also add a new gender to the switch statement in the Xtension method.
     public enum Extra{REGULAR, LIMITED, SALE, EXTRAVAGANT}
-    public enum OrderStatus{BUSY, SEND, DELAYED}
+    public enum OrderStatus{BUSY, SEND, DELAYED, RECEIVED}
 }
