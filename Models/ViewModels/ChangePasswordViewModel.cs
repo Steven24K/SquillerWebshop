056d526119fbd2cd26a43f8449892cf733e725d5
Steven <s.koerts2@gmail.com>
2018-01-05 16:49:53 +0100

    Improved form validation + form check

--- Models/ViewModels/ChangePasswordViewModel.cs
+++ Models/ViewModels/ChangePasswordViewModel.cs
@@ -6,6 +6,7 @@ namespace Webshop.Models
         [Required(ErrorMessage = "Please fill in your current password")]
         public string Password{get;set;}
         [Required(ErrorMessage = "Fill in new password")]
+        [MinLength(7, ErrorMessage = "Password must be atleast 7 characters")]
         public string NewPassword{get;set;}
         [Required(ErrorMessage = "Please confirm your new password")]
         [Compare("NewPassword", ErrorMessage="Password does not match")]
--- Models/ViewModels/ContactViewModel.cs
+++ Models/ViewModels/ContactViewModel.cs
@@ -6,6 +6,7 @@ namespace Webshop.Models.ViewModels
     public class ContactViewModel
     {
         [Required(ErrorMessage = "Please select a Title")]
+        [MaxLength(20, ErrorMessage = "Title cannot be longer than 20 characters")]
         public string Title{get;set;}
         [Required(ErrorMessage = "Name is Required!")]
         public string Name{get;set;}
@@ -15,13 +16,16 @@ namespace Webshop.Models.ViewModels
         [EmailAddress(ErrorMessage = "This does not look like an email adress")]
         public string Email{get;set;}
         [Required(ErrorMessage = "Phonenumber is Required")]
+        [Phone(ErrorMessage = "This does not look like a Phonenumber")]
         public int Phonenumber{get; set;}
         [Required(ErrorMessage = "Description is Required!")]
+        [MaxLength(512, ErrorMessage = "Message cannot be longer that 512 characters")]
         public string Description{get; set;}
         [Required(ErrorMessage = "Subject is Required")]
         public string Subject{get; set;}
 
         [Required()]
+        [RegularExpression("([0-9]+)")]
         public int Ordernumber{get; set;}
         
     }
